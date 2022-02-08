945059ae0cbfcc5fbac53a525cd4d77365b2d90f
milkaotieno <milka.otieno@student.moringaschool.com>
2022-02-08 15:43:35 +0300

    update form compnent logic

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Bestquotes } from '../bestquotes';
+
 
 @Component({
   selector: 'app-quote-form',
@@ -6,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+
+  
+  newBestquote = new Bestquotes("","", new Date(), " ", 0, 0);
+@Output() addGoal = new EventEmitter<Bestquotes>();
+
+  submitGoal(){
+this.addGoal.emit(this.newBestquote);
+  }
 
   constructor() { }
 
