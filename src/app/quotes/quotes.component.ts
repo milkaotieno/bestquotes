65f4e4b63e8701fcde25f1c7dbb3fbde4caad32c
milkaotieno <milka.otieno@student.moringaschool.com>
2022-02-08 17:14:22 +0300

    add changes

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+//import { Quote } from '@angular/compiler';
+import { Component, OnInit, Output } from '@angular/core';
 import { Bestquotes } from '../bestquotes';
 
 @Component({
@@ -12,7 +13,10 @@ export class QuotesComponent implements OnInit {
     new Bestquotes("Peace begins with a smile", "Mother Teresa", new Date(20220207), "Milka", 0, 0),
     new Bestquotes("When you don't have anything, then you have everything.", "Mother Teresa", new Date(2001001), "Motieno", 0, 0),
     new Bestquotes("The only true wisdom is in knowing you know nothing.", "Socrates", new Date(20170908), "Milka", 0, 0)
-  ]
+  ];
+submitBestquote(Bestquotes:any){
+  Bestquotes.author = new Bestquotes().author
+}
 
 
   constructor() { }
